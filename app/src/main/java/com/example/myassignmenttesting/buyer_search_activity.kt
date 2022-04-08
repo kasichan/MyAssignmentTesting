a412500644163d17ea5df2e0f7616c4b1900aaee
yyytttyt <leeyt-pm19@student.tarc.edu.my>
2022-04-09 02:30:21 +0800

    yt_9/4/2022

---
+++ app/src/main/java/com/example/myassignmenttesting/buyer_search_activity.kt
@@ -0,0 +1,11 @@
+package com.example.myassignmenttesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class buyer_search_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.buyer_search)
+    }
+}
