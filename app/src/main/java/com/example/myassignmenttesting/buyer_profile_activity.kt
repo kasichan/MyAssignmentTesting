101030ff561801aade609cd612485fcb3e13a734
hey awak <70712101+FlavouredSocks@users.noreply.github.com>
2022-04-10 03:02:55 +0800

    Nav Bar complete

--- app/src/main/java/com/example/myassignmenttesting/buyer_profile_activity.kt
+++ app/src/main/java/com/example/myassignmenttesting/buyer_profile_activity.kt
@@ -1,11 +1,20 @@
 package com.example.myassignmenttesting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class buyer_profile_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.buyer_profile)
+
+        val connectChatroom = findViewById<Button>(R.id.chat_button)
+        connectChatroom.setOnClickListener {
+            val Intent = Intent(this, buyer_chatroom_activity::class.java)
+            startActivity(Intent)
+        }
+
     }
 }
