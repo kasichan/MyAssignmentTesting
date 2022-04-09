d1459c39ba4ff8171979be8f43c8f8dd37081416
AlvinChok <alvinchok2001@gmail.com>
2022-04-09 22:18:05 +0800

    saession ok profile ok register+login ok

--- app/src/main/java/com/example/myassignmenttesting/LoggedIn.kt
+++ app/src/main/java/com/example/myassignmenttesting/LoggedIn.kt
@@ -5,37 +5,55 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.buyer_profile.*
 
 class LoggedIn : AppCompatActivity() {
     private lateinit var db : FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.buyer_homepage)
+        setContentView(R.layout.buyer_profile)
+
         val sharePref= this?.getPreferences(Context.MODE_PRIVATE)?:return
-        val isLogin = sharePref.getString("Email","1")
-        //val email = intent.getStringExtra("email")
+        val isLogin = sharePref.getString("email","1")
+        var status = ""
 
-        if(isLogin=="1"){
-            with(sharePref.edit())
-            {
-                putString("Email",email)
-                apply()
+        if(isLogin=="1")
+        {
+            var email = intent.getStringExtra("email")
+
+            if(email!=null){
+                status = "loggedIn"
+                setText(email)
+                with(sharePref.edit())
+                {
+                    putString("Email",email)
+                    apply()
+                }
             }
-        }
-        else{
-//            var intent = Intent(this,MainActivity::class.java)
-//            startActivity(intent)
-//            finish()
-            setText(isLogin)
-        }
 
+        }
+        else
+        {
+            var intent = Intent(this,buyer_user_login_activity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
     }
 
-    private fun setText(email:String){
+    private fun setText(email:String?){
         db = FirebaseFirestore.getInstance()
-        db.collection("User").document(email).get()
-            .addOnSuccessListener {
-                tasks->
-            }
+
+        if(email!=null){
+            db.collection("User").document(email).get()
+                .addOnSuccessListener {
+                        tasks->
+                    username.text = tasks.get("username").toString()
+
+
+                }
+        }
+
     }
+
 }
--- app/src/main/java/com/example/myassignmenttesting/ui/main/RegisterActivity.kt
+++ app/src/main/java/com/example/myassignmenttesting/ui/main/RegisterActivity.kt
@@ -2,16 +2,24 @@ package com.example.myassignmenttesting.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.example.myassignmenttesting.databinding.RegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: RegisterBinding
+    private lateinit var actionBar: ActionBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = RegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        actionBar = supportActionBar!!
+        actionBar.title = "Register"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
     }
+
 }
