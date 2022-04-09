0db51d0558a082de3768d8e4607a7f8e7a607567
yyytttyt <leeyt-pm19@student.tarc.edu.my>
2022-04-10 00:53:53 +0800

    latest/10/4

--- app/src/main/java/com/example/myassignmenttesting/buyer_profile_activity.kt
+++ app/src/main/java/com/example/myassignmenttesting/buyer_profile_activity.kt
@@ -1,11 +1,31 @@
 package com.example.myassignmenttesting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class buyer_profile_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.buyer_profile)
+
+        val connectChatRoom = findViewById<Button>(R.id.button)
+        connectChatRoom.setOnClickListener{
+            val Intent = Intent(this, buyer_chatroom_activity::class.java)
+            startActivity(Intent)
+        }
+
+        val setting = findViewById<Button>(R.id.button1)
+        setting.setOnClickListener{
+            val Intent = Intent(this, edit_profile_activity::class.java)
+            startActivity(Intent)
+        }
+
+        val connectSellerCenter = findViewById<Button>(R.id.seller_center)
+        connectSellerCenter.setOnClickListener{
+            val Intent = Intent(this, seller_dashboard_activity::class.java)
+            startActivity(Intent)
+        }
     }
 }
